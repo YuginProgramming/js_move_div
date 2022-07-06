bcec8fb029611268e22ffaaee50ced0205c988c4
YuginProgramming <63294494+YuginProgramming@users.noreply.github.com>
2022-07-06 17:29:47 +0300

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -2,18 +2,20 @@ const pacman = document.querySelector('.pacman');
 let left = 0;
 let right = 0;
 
+window.addEventListener ('load', () => {
+   pacman.style.position = 'absolute';
+
+
+} );
+
 document.addEventListener ('keydown', (event) => {
    if(event.key === 'ArrowLeft'){
       right+= 50;
       pacman.style.right =`${right}px`;
-      return; 
-   }; 
    
-   if(event.key === 'ArrowRight'){
-      left+= 50;
-      pacman.style.left =`${left}px`;
-      return; 
-   }; 
-
-}
+   }   else(event.key === 'ArrowRight')
+         right-= 50;
+         pacman.style.right =`${right}px`;
+         console.log(event);
+   }
 )
